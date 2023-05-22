0eea2312ecce34215c3f7ed51e80c47c9df16c8a
theshrish46 <theshrish46@gmail.com>
2023-05-22 22:07:55 +0530

    created Nabvbar

---
+++ src/components/PreNavbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './../styles/PreNavbar.css'
+
+
+const PreNavbar = () => {
+  return (
+    <div className='preNav'>
+
+    </div>
+  )
+}
+
+export default PreNavbar
